840108de95e5d36d3c9d79102eae9e40f6090da6
levi davis <levidavis@pursuit.org>
2019-10-01 15:06:06 -0400

    sets up image picker access

--- lab-image-picker/Controllers/ImageViewController.swift
+++ lab-image-picker/Controllers/ImageViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Photos
 
 class ImageViewController: UIViewController {
     
@@ -15,20 +16,75 @@ class ImageViewController: UIViewController {
             imageView.image = image
         }
     }
+    
+    var photoLibraryAccess = false
 
     @IBOutlet weak var imageView: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
         // Do any additional setup after loading the view.
     }
     
     
     @IBAction func changeImageButtonPressed(_ sender: UIButton) {
+        checkPhotoLibraryAccess()
+        let imagePickerViewController = UIImagePickerController()
+        imagePickerViewController.delegate = self
+        imagePickerViewController.sourceType = .photoLibrary
         
+        if photoLibraryAccess {
+            present(imagePickerViewController, animated: true, completion: nil)
+        } else {
+            let alertVC = UIAlertController(title: "No Access", message: "Photo library access is required to use this app.", preferredStyle: .alert)
+            
+            alertVC.addAction(UIAlertAction (title: "Ok", style: .default, handler: { UIAlertAction in
+                self.photoLibraryAccess = true
+                
+                self.present(imagePickerViewController, animated: true, completion: nil)
+            }))
+            
+            
+            alertVC.addAction(UIAlertAction(title: "No", style: .destructive, handler: { UIAlertAction in
+                self.photoLibraryAccess = false
+                
+            }))
+            self.present(alertVC, animated: true, completion: nil)
+        }
+        
+    }
+    
+    private func checkPhotoLibraryAccess() {
+        let status = PHPhotoLibrary.authorizationStatus()
+        
+        switch status {
+        case .authorized:
+            print(status)
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization({status in
+                switch status {
+                case .authorized:
+                    self.photoLibraryAccess = true
+                case .denied:
+                    self.photoLibraryAccess = false
+                case .notDetermined:
+                    print("not determined")
+                case .restricted:
+                    print("restricted")
+                @unknown default:
+                    print("error")
+                }
+            })
+        case .denied:
+            let alertVC = UIAlertController(title: "Denied", message: "Photo album access is required to use this app. Please change your preference in settings", preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction (title: "Ok", style: .default, handler: nil))
+            self.present(alertVC, animated: true, completion: nil)
+        case .restricted:
+            print("restricted")
+        @unknown default:
+            print("error")
+        }
     }
     
 }
